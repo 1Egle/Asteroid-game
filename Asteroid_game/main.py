6553a6ed8edca387c0774da28212fa576669b9a1
Egle Salaseviciute <egle.salaseviciute.7@gmail.com>
2025-08-20 19:39:30 +0200

    2: Added a spaceship

--- Asteroid_game/main.py
+++ Asteroid_game/main.py
@@ -1,18 +1,27 @@
 import pygame
 from constants import *
+from player import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+     
         screen.fill("black")
-        pygame.display.flip()
-    print("Starting Asteroids!")
-    print("Screen width: 1280")
-    print("Screen height: 720")
+        player.draw(screen)
 
+        pygame.display.flip()
+        dt += clock.tick(60) / 1000
+
+
+    
 
 if __name__ == "__main__":
     main()
